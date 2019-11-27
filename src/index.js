0ea754ff52607bd0358ef0a3038b9c9ed2b58e98
Alex Wilmer <djazium@gmail.com>
2019-11-26 20:42:44 -0500

    fix: Move declaration up

--- src/index.js
+++ src/index.js
@@ -13,6 +13,75 @@ if (!fs.existsSync(data_dir)) {
 }
 
 async function main() {
+  async function getData(fsa) {
+    await page.goto(CLINIC_FINDER_URL, {
+      waitUntil: 'networkidle2',
+      timeout: 3000000,
+    })
+
+    let s = t => new Promise(r => setTimeout(r, t))
+
+    await page.waitForSelector('#search-input')
+    await page.focus('#search-input')
+    await page.keyboard.type(fsa)
+    await page.focus('#search-input')
+    await page.waitForSelector('.pac-container')
+
+    await s(2000)
+    await page.keyboard.press('ArrowDown')
+    await s(1000)
+    await page.click('body')
+    await s(1000)
+    await page.focus('#search-button')
+    await page.click('#search-button')
+
+    await s(2000)
+
+    let data = await page.evaluate(() => {
+      let searchResults = Array.from(
+        document.querySelectorAll('.search-result'),
+      )
+      return searchResults.map(result => {
+        let name = result.querySelector('label.sb.purple').innerText
+        let city = result.querySelector('label.ssb.black-font').innerText
+        let address = result.querySelector('p.sr').innerText
+
+        let details = result.querySelector(
+          '.row:nth-child(2) > div:last-child .col:first-child > a',
+        ).href
+
+        let directions = result.querySelector(
+          '.row:nth-child(2) > div:last-child .col:last-child > a',
+        ).href
+
+        return {
+          name,
+          city,
+          address,
+          details,
+          directions,
+        }
+      })
+    })
+
+    // save data as JSON
+
+    await new Promise((resolve, reject) => {
+      fs.writeFile(
+        `./${data_dir}/` + fsa + '-data.json',
+        JSON.stringify(data, null, 2),
+        err => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+            console.log(fsa + ' Done.')
+          }
+        },
+      )
+    })
+  }
+
   const fsas = await loadFsas(FSA_FILE)
 
   try {
@@ -21,75 +90,6 @@ async function main() {
     })
 
     const page = await browser.newPage()
-
-    async function getData(fsa) {
-      await page.goto(CLINIC_FINDER_URL, {
-        waitUntil: 'networkidle2',
-        timeout: 3000000,
-      })
-
-      let s = t => new Promise(r => setTimeout(r, t))
-
-      await page.waitForSelector('#search-input')
-      await page.focus('#search-input')
-      await page.keyboard.type(fsa)
-      await page.focus('#search-input')
-      await page.waitForSelector('.pac-container')
-
-      await s(2000)
-      await page.keyboard.press('ArrowDown')
-      await s(1000)
-      await page.click('body')
-      await s(1000)
-      await page.focus('#search-button')
-      await page.click('#search-button')
-
-      await s(2000)
-
-      let data = await page.evaluate(() => {
-        let searchResults = Array.from(
-          document.querySelectorAll('.search-result'),
-        )
-        return searchResults.map(result => {
-          let name = result.querySelector('label.sb.purple').innerText
-          let city = result.querySelector('label.ssb.black-font').innerText
-          let address = result.querySelector('p.sr').innerText
-
-          let details = result.querySelector(
-            '.row:nth-child(2) > div:last-child .col:first-child > a',
-          ).href
-
-          let directions = result.querySelector(
-            '.row:nth-child(2) > div:last-child .col:last-child > a',
-          ).href
-
-          return {
-            name,
-            city,
-            address,
-            details,
-            directions,
-          }
-        })
-      })
-
-      // save data as JSON
-
-      await new Promise((resolve, reject) => {
-        fs.writeFile(
-          `./${data_dir}/` + fsa + '-data.json',
-          JSON.stringify(data, null, 2),
-          err => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve()
-              console.log(fsa + ' Done.')
-            }
-          },
-        )
-      })
-    }
 
     for (let f of fsas) {
       await getData(f)
